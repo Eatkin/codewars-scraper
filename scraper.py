8a1bb14dd9282746df7676c3720d4dac7a714f5d
Edward Atkin <edward.atkin@live.com>
2023-04-27 13:56:06 +0100

    Update comments in scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,6 @@
 '''Codewars scraper
 Finds all my codewars solutions, saves and organises them
-Appends a link to the problems at the end of the code'''
+Appends a link to the problems at the beginning of the code'''
 
 from bs4 import BeautifulSoup
 import string
@@ -24,21 +24,18 @@ language_comments = {
 }
 
 # Create a translator which will remove all punctuation and replace spaces with underscores
+# Retains any hyphens
 translator = str.maketrans("", "", string.punctuation)
 translator[ord(" ")] = "_"
 translator[ord("-")] = "-"
 '''
-Page format:
+HTML page format:
 All solution are contained in a div with class "items-list"
 Each individual solution is contained within a div with class "list-item-solutions"
 The difficulty is contained in a div of class "item-title", alongside the problem name and link
 The language is displayed in a h6 tag with the content of the form "[Language]:" (e.g. "Python:")
 We'll need to remove the colon from the end of the language
 Finally the problem itself is contained within a code block
-This is all formatted nicely using spans, so they need to be removed
-Fortunately beautiful soup does this automatically so no problem
-The only remaining problem to be solved is when there are multiple solutions
-Which I'm sure we can just solve using a loop through each code element found
 '''
 
 # Get the page
@@ -47,14 +44,18 @@ try:
     with open("Eatkin_Codewars_toscrape.html", "r") as f:
         soup = BeautifulSoup(f.read(), "html.parser")
 except FileNotFoundError:
-    print("File not found")
+    print(
+        "File not found, please make sure you have downloaded the page and placed it in the same directory as this script and set the filename variable correctly"
+    )
     exit()
 
 # Now find the div with class "items-list"
 main_content = soup.find("div", class_="items-list")
 
+# Get the solutions
 solutions = main_content.find_all("div", class_="list-item-solutions")
 
+# Loop through all solutions
 for solution in solutions:
     # Get the difficulty, problem name and link
     head_data = solution.find("div", class_="item-title")
@@ -71,13 +72,6 @@ for solution in solutions:
     solution_code = solution.find_all("code")
     solution_code.reverse()
     for version, code_block in enumerate(solution_code):
-        # Now we need to save the solution to a file with a comment appended to the top with the problem link
-        # The filename can be the problem name with spaces replaced with underscores and the filetype appended
-        # We'll categorise the solutions by language in subfolders
-        # We'll then append the difficulty to the end of the filename
-        # We can also append a version number if version > 0
-        # I'm having a migraine so and I can't really see what I'm typing but let's try anyway
-
         # Construct the filename
         filename = problem_name
         if version > 0:
