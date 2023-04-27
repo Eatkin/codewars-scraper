64d92481e92747c0c57cb67f50dd0eda92b15f38
Edward Atkin <edward.atkin@live.com>
2023-04-27 13:39:02 +0100

    Update scraper to actually scrape all necessary data. Auto-categorise code into files and folders.

--- scraper.py
+++ scraper.py
@@ -2,23 +2,31 @@
 Finds all my codewars solutions, saves and organises them
 Appends a link to the problems at the end of the code'''
 
+from bs4 import BeautifulSoup
+import string
+import os
+
 # Set up filetypes dictionary for each language
+# Ensure the languages are lowercase
 language_filetypes = {
-    "C": ".c",
-    "Javascript": ".js",
-    "Python": ".py",
-    "Shell": ".sh",
+    "c": ".c",
+    "javascript": ".js",
+    "python": ".py",
+    "shell": ".sh",
 }
 
 # Set up comments dictionary for each language
 language_comments = {
-    "C": "//",
-    "Javascript": "//",
-    "Python": "#",
-    "Shell": "#",
+    "c": "//",
+    "javascript": "//",
+    "python": "#",
+    "shell": "#",
 }
 
-from bs4 import BeautifulSoup
+# Create a translator which will remove all punctuation and replace spaces with underscores
+translator = str.maketrans("", "", string.punctuation)
+translator[ord(" ")] = "_"
+translator[ord("-")] = "-"
 '''
 Page format:
 All solution are contained in a div with class "items-list"
@@ -29,6 +37,8 @@ We'll need to remove the colon from the end of the language
 Finally the problem itself is contained within a code block
 This is all formatted nicely using spans, so they need to be removed
 Fortunately beautiful soup does this automatically so no problem
+The only remaining problem to be solved is when there are multiple solutions
+Which I'm sure we can just solve using a loop through each code element found
 '''
 
 # Get the page
@@ -49,22 +59,47 @@ for solution in solutions:
     # Get the difficulty, problem name and link
     head_data = solution.find("div", class_="item-title")
     difficulty = head_data.find("span").text
-    print(difficulty)
     problem_name = head_data.find("a").text
-    print(problem_name)
     problem_link = head_data.find("a")["href"]
-    print(problem_link)
 
     # The h6 is directly below
     language = solution.find("h6").text[:-1]
-    print(language)
 
     # Now the solution itself
-    solution_code = solution.find("code").text
-    print(solution_code)
+    # There may be multiple solutions, so reverse the order going from oldest first
+    # This means we can save the refactors as v2, v3, etc.
+    solution_code = solution.find_all("code")
+    solution_code.reverse()
+    for version, code_block in enumerate(solution_code):
+        # Now we need to save the solution to a file with a comment appended to the top with the problem link
+        # The filename can be the problem name with spaces replaced with underscores and the filetype appended
+        # We'll categorise the solutions by language in subfolders
+        # We'll then append the difficulty to the end of the filename
+        # We can also append a version number if version > 0
+        # I'm having a migraine so and I can't really see what I'm typing but let's try anyway
 
-    # Now we need to save the solution to a file with a comment appended to the top with the problem link
-    # The filename can be the problem name with spaces replaced with underscores and the filetype appended
-    # We'll categorise the solutions by language in subfolders
-    # We'll then append the difficulty to the end of the filename
-    break
+        # Construct the filename
+        filename = problem_name
+        if version > 0:
+            filename += f"-v{version + 1}"
+        filename += f"-{difficulty}"
+        filename = filename.translate(translator)
+        filename += language_filetypes[language.lower()]
+
+        # Construct the filepath
+        directory = f"{language}"
+        filepath = f"{language}/{filename}"
+
+        # Construct the comment
+        comment = f"{language_comments[language.lower()]} {problem_link}"
+
+        # Now write the file
+        # Create directory if necessary
+        if not os.path.exists(directory):
+            os.makedirs(directory)
+
+        # Check for file existence first
+        if not os.path.exists(filepath):
+            with (open(filepath, "w")) as f:
+                f.write(comment + "\n")
+                f.write(code_block.text)
